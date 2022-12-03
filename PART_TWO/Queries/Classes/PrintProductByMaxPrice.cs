639b2cdc505dc808f246bf45e529e2940a5c41c6
kazmi <costellationoblivion@gmail.com>
2022-12-03 13:42:31 +0200

    Добавьте файлы проекта.

---
+++ PART_TWO/Queries/Classes/PrintProductByMaxPrice.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+// Показати канцтовари з максимальною собівартістю одиниці
+internal class PrintProductByMaxPrice : IQuery
+{
+    public SqlConnection connection { get; }
+
+    public PrintProductByMaxPrice(SqlConnection connection) => this.connection = connection;
+
+
+    public async Task Query() // приймається підключення і назва таблиці
+    {
+        var command = new SqlCommand
+        {
+            Connection = connection, // підключення передається в команду
+            CommandText = "SELECT [Тип канцтовару],MIN([Кількість]) AS [Мін. кількість] FROM [Канцтовари] GROUP BY [Тип канцтовару]" // текст команди
+        };
+        var reader = await command.ExecuteReaderAsync(); // результат запиту записується в SQLDataReader
+
+        if (reader.HasRows) // якщо було записано хоча б один рядок
+        {
+            // Витягуються імена стовпців
+            string col1 = reader.GetName(0);
+            string col2 = reader.GetName(1);
+
+            // Константний займаючий простір(для коректного відображення в консолі в форматі таблиці)
+            col1 = col1.PadRight(30, ' ');
+            col2 = col2.PadRight(10, ' ');
+
+            Console.WriteLine($"{col1}\t{col2}"); // вивід імен стовпців
+
+
+            while (await reader.ReadAsync())
+            {
+                // Витягуються значення стовпців
+                col1 = (string)reader.GetValue(0);
+                col2 = Convert.ToString(reader.GetValue(1));
+
+                // Константний займаючий простір(для коректного відображення в консолі в форматі таблиці)
+                col1 = col1.PadRight(30, ' ');
+                col2 = col2.PadRight(10, ' ');
+
+                Console.WriteLine($"{col1}\t{col2}"); // вивід значень стовпців
+            }
+        }
+
+        await reader.CloseAsync();
+    }
+}
